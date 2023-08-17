c852e024bf76be674d3b4a812df76b2aa8b4be91
uomaep <uomaep@gmail.com>
2023-08-18 02:31:13 +0900

    feat: if fail set user data to db then logging local file
    처음 유저가 접속 했을 때 만약 데이터 베이스 문제로 저장을 하지 못했을 시
    플러그인 폴더의 failedUserData.yml 파일에 등록안된 유저를 기록함.
    
    - 예시
    1:
      nickname: uomaep
      uuid: abcdefghijklmnopqrstuvwxyz
      timestamp: 2023-08-18T02:35:00

--- src/main/java/com/uomaep/cashandmileageshop/Main.kt
+++ src/main/java/com/uomaep/cashandmileageshop/Main.kt
@@ -2,6 +2,7 @@ package com.uomaep.cashandmileageshop
 
 import com.uomaep.cashandmileageshop.commands.CashCommand
 import com.uomaep.cashandmileageshop.commands.SetItemCommand
+import com.uomaep.cashandmileageshop.listeners.SetUserAtFirstJoin
 import com.uomaep.cashandmileageshop.utils.DatabaseManager
 import com.uomaep.mileageandmileageshop.commands.MileageCommand
 import org.bukkit.plugin.java.JavaPlugin
@@ -9,6 +10,8 @@ import org.bukkit.plugin.java.JavaPlugin
 class Main : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
+        createPluginFolder()
+
         DatabaseManager.connect()
 
         // 명령어 등록
@@ -17,11 +20,19 @@ class Main : JavaPlugin() {
         getCommand("아이템등록")?.setExecutor(SetItemCommand())
 
         // 이벤트 등록
+        server.pluginManager.registerEvents(SetUserAtFirstJoin(this), this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
 
         DatabaseManager.disconnect()
+    }
+
+    fun createPluginFolder() {
+        val pluginFolder = dataFolder
+        if (!pluginFolder.exists()) {
+            pluginFolder.mkdir()
+        }
     }
 }
--- src/main/java/com/uomaep/cashandmileageshop/listeners/SetUserAtFirstJoin.kt
+++ src/main/java/com/uomaep/cashandmileageshop/listeners/SetUserAtFirstJoin.kt
@@ -1,16 +1,59 @@
 package com.uomaep.cashandmileageshop.listeners
 
 import com.uomaep.cashandmileageshop.utils.DatabaseManager
+import org.bukkit.Bukkit
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.io.IOException
+import java.time.LocalDateTime
 
-class SetUserAtFirstJoin: Listener {
+class SetUserAtFirstJoin(val plugin: JavaPlugin): Listener {
+
     @EventHandler
     fun setUserToDatabase(e: PlayerJoinEvent) {
         val player = e.player
         if(player.hasPlayedBefore()) return
 
-        DatabaseManager.insert("insert into user (uuid, cash, mileage) values ('${player.uniqueId}', 0, 0);")
+        if(!DatabaseManager.insert("insert into user (uuid, cash, mileage) values ('${player.uniqueId}', 0, 0);")) {
+            Bukkit.getLogger().severe("데이터베이스에 새로운 플레이어의 정보를 추가하는데 실패했습니다.")
+            if(!saveFailedUserDataToFile(player)) {
+                Bukkit.getLogger().severe("플레이어의 정보를 파일에 저장하는데 실패했습니다.")
+            }
+        }
+    }
+
+    private fun saveFailedUserDataToFile(player: Player): Boolean {
+        val fileName = "failedUserData.yml"
+        val configFile = File(plugin.dataFolder, fileName)
+        val config = YamlConfiguration()
+
+        val failedUserData = hashMapOf(
+            "nickname" to player.name,
+            "uuid" to player.uniqueId.toString(),
+            "timestamp" to LocalDateTime.now()
+        )
+
+        var userCount = 1
+        while(config.contains(userCount.toString())) userCount++
+
+        val userSection = config.createSection(userCount.toString())
+
+        failedUserData.forEach { (key, value) ->
+            userSection.set(key, value)
+        }
+
+        try {
+            config.save(configFile)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return false
+        }
+
+        return true
     }
 }
