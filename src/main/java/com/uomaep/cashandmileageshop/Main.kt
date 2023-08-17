a157426b267098bcdf0a56cd76f98fe40c1c69bb
uomaep <uomaep@gmail.com>
2023-08-18 01:39:27 +0900

    feat: /아이템등록 command

--- src/main/java/com/uomaep/cashandmileageshop/Main.kt
+++ src/main/java/com/uomaep/cashandmileageshop/Main.kt
@@ -1,6 +1,7 @@
 package com.uomaep.cashandmileageshop
 
 import com.uomaep.cashandmileageshop.commands.CashCommand
+import com.uomaep.cashandmileageshop.commands.SetItemCommand
 import com.uomaep.cashandmileageshop.utils.DatabaseManager
 import com.uomaep.mileageandmileageshop.commands.MileageCommand
 import org.bukkit.plugin.java.JavaPlugin
@@ -13,6 +14,7 @@ class Main : JavaPlugin() {
         // 명령어 등록
         getCommand("캐시")?.setExecutor(CashCommand())
         getCommand("마일리지")?.setExecutor(MileageCommand())
+        getCommand("아이템등록")?.setExecutor(SetItemCommand())
 
         // 이벤트 등록
     }
---
+++ src/main/java/com/uomaep/cashandmileageshop/commands/SetItemCommand.kt
@@ -0,0 +1,52 @@
+package com.uomaep.cashandmileageshop.commands
+
+import com.uomaep.cashandmileageshop.utils.DatabaseManager
+import com.uomaep.kotlintestplugin.utils.ItemUtil
+import org.bukkit.Material
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class SetItemCommand: CommandExecutor {
+    companion object {
+        private fun sendFailMessage(player: Player, message: String) {
+            player.sendMessage("§c[§e!§c] $message")
+        }
+
+        private fun sendSuccessMessage(player: Player, message: String) {
+            player.sendMessage("§a[§e!§a] $message")
+        }
+    }
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        val player = sender as Player
+
+        if(!player.isOp) {
+            player.sendMessage("Unknown command. Type \"/help\" for help.")
+            return false
+        }
+
+        if(args?.size == 0) {
+            sendFailMessage(player, "아이템 이름을 입력해주세요.")
+            return false
+        }
+
+        val item = player.inventory.itemInMainHand
+
+        if(item.type == Material.AIR) {
+            sendFailMessage(player, "손에 아이템을 들고 명령어를 사용해주세요.")
+            return false
+        }
+
+        val itemName = args?.get(0)
+        val serializedItem = ItemUtil.serialize(item)
+
+        if(!DatabaseManager.insert("insert into item (name, `\u202Aitem_info`) values('${itemName}', '${serializedItem}');")) {
+            sendFailMessage(player, "이미 등록된 이름의 아이템 이거나 등록 중 문제가 발생했습니다.")
+            return false
+        }
+
+        sendSuccessMessage(player, "아이템이 등록되었습니다.")
+        return true
+    }
+}
